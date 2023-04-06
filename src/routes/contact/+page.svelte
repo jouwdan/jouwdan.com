70734edaea95bb5ef063927290edddc7645d535e
Jordan Harrison <contact@jordanharrison.ie>
2023-04-06 21:38:38 +0100

    feat: contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<div class="container mx-auto p-16 rounded-xl border border-surface-700 bg-surface-800">
+  <h2 class="font-bold mb-4">Get in Touch</h2>
+  <form action="#" method="POST" class="space-y-1">
+    <label class="label" for="name">Name</label>
+    <input class="input" type="text" name="name" id="name" placeholder="Your Name" />
+    <label class="label" for="email">Email</label>
+    <input class="input" type="email" name="_replyto" id="email" placeholder="your@email.com" />
+    <label class="label" for="subject">Subject</label>
+    <input class="input" type="text" name="subject" id="subject" placeholder="Subject" />
+    <label class="label" for="message">Message</label>
+    <textarea class="input" name="message" rows="4" id="message" placeholder="Insert your message here..."></textarea>
+    <button class="btn variant-ghost-primary" type="submit">Send</button>
+</div>
