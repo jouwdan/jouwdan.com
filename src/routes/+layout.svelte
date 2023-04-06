012142f0d6a3e96a279865cc763c82042f9d43d7
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-04-06 14:23:53 +0100

    feat: init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import '$lib/theme.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+
+	$: currentRoute = $page.url.pathname;
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar background="bg-surface-900" class="container mx-auto my-4">
+			<svelte:fragment slot="lead">
+				<a href="/">
+				<Avatar src="images/logo.png" rounded="rounded-xl" />
+			</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+				class="btn btn-sm hover:variant-ghost-primary transition-all duration-200"
+				href="/"
+				class:variant-ghost-surface={currentRoute !== "/"}
+				class:variant-ghost-primary={currentRoute === "/"}
+			>
+				Home
+			</a>
+			<a
+			class="btn btn-sm hover:variant-ghost-primary transition-all duration-200"
+			href="/blog"
+			class:variant-ghost-surface={!currentRoute.includes("/blog")}
+			class:variant-ghost-primary={currentRoute.includes("/blog")}
+		>
+			Blog
+		</a>
+		<hr class="divider-vertical h-6">
+				<a
+					class="btn btn-sm variant-soft-surface hover:variant-soft-primary"
+					href="https://twitter.com/jouwdan"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Icon icon="mdi:twitter" class="text-xl" />
+				</a>
+				<a
+					class="btn btn-sm variant-soft-surface hover:variant-soft-primary"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+				<Icon icon="mdi:github" class="text-xl" />
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div
+	class="container px-12 rounded-xl h-[36rem] mx-auto flex justify-center items-center bg-gradient-to-br from-primary-500 to-secondary-500 text-surface-900"
+>
+		<div class="flex">
+			<div class="my-auto w-1/2 pl-24">
+				<h1 class="font-bold mb-4">Hey, I'm Jordan.</h1>
+				<h3>
+					I'm a creative full stack developer based in Ireland with have a passion for web tech &
+					open source. Oh, and i'm a total tech nerd.
+				</h3>
+			</div>
+			<img src="images/waving-avatar.png" alt="avatar of Jordan waving" class="mx-auto pb-12" />
+		</div>
+</div>
