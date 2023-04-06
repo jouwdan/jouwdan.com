b204331430d032bf8fb04d94452431aa964b1c8c
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-04-06 17:18:09 +0100

    fix: responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div
-	class="container px-12 rounded-xl h-[36rem] mx-auto flex justify-center items-center bg-gradient-to-br from-primary-500 to-secondary-500 text-surface-900"
+	class="container px-12 rounded-xl py-12 mx-auto justify-center items-center bg-gradient-to-br from-primary-500 to-secondary-500 text-surface-900"
 >
-		<div class="flex">
-			<div class="my-auto w-1/2 pl-24">
+		<div class="flex flex-col-reverse lg:flex-row">
+			<div class="my-auto lg:w-1/2 lg:pl-24">
 				<h1 class="font-bold mb-4">Hey, I'm Jordan.</h1>
 				<h3>
 					I'm a creative full stack developer based in Ireland with have a passion for web tech &
