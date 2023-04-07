feb66219a77a7df1a3861c6de5ca1e66ad9dda6c
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-04-07 12:14:32 +0100

    feat: add tutors to portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,2 +1,14 @@
-
 <h1 class="font-bold pt-4 pb-8">Portfolio</h1>
+  <div class="p-16 rounded-xl border border-surface-700 bg-surface-800">
+    <div class="block lg:flex items-center">
+      <img src="/images/tutors.png" alt="Tutors" class="w-full lg:w-1/4" />
+      <div class="flex-1">
+        <div class="block pl-12">
+          <h2 class="font-bold">Tutors Open Source Project</h2>
+          <h3>Maintainer</h3>
+          <p>Tutors is a collection of open source components & services supporting the creation of transformative learning experiences using open web standards.</p>
+          <a class="btn variant-ghost-primary mt-4" href="https://tutors.dev" target="_blank" rel="noreferrer">Visit Tutors</a>
+        </div>
+      </div>
+    </div>
+</div>
