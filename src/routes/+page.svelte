a494b04e4f3199235f15b116f85bd849181b3762
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-04-07 09:20:24 +0100

    chore: wording

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,9 @@
 >
 		<div class="flex flex-col-reverse lg:flex-row">
 			<div class="my-auto lg:w-1/2 lg:pl-24">
-				<h1 class="font-bold mb-4">Hey, I'm Jordan.</h1>
+				<h1 class="font-bold mb-4">Hey, I'm Jordan!</h1>
 				<h3>
-					I'm a creative full stack developer based in Ireland with a passion for web tech &
-					open source. Oh, and i'm a total tech nerd.
+					I'm a creative full stack developer based in Ireland. I love tinkering with web tech and am a big fan of open source. Also, I'm a bit of a tech nerd (but hey, who isn't these days?).
 				</h3>
 				<a class="btn variant-ghost-primary mt-4" href="/about">About Me</a>
 				<a class="btn variant-ghost-secondary mt-4" href="/contact">Get in Touch</a>
