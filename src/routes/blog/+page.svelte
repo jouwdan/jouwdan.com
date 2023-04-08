b9da93867ed57c7123193e6e633e3495a8c71c41
Jordan Harrison <contact@jordanharrison.ie>
2023-04-08 09:40:36 +0100

    fix: grid

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <h1 class="font-bold pt-4 pb-8">Blog</h1>
-<div class="block lg:grid lg:grid-cols-3">
+<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
 	{#each data.posts as post}
 		<BlogCard post={post} />
 	{/each}
