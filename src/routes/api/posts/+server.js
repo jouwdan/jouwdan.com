87e79ba8f61769a686e51eb8f6d3c3d19fe3b03b
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-04-10 13:18:11 +0100

    fix: remove unnecessary stuffs

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,12 +1,30 @@
-import { fetchMarkdownPosts } from '$lib/utils/fetchMarkdownPosts'
-import { json } from '@sveltejs/kit'
+import { json } from '@sveltejs/kit';
+
+const fetchMarkdownPosts = async () => {
+	const allPostFiles = import.meta.glob('$lib/blog/*.md');
+	const iterablePostFiles = Object.entries(allPostFiles);
+
+	const allPosts = await Promise.all(
+		iterablePostFiles.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const postPath = path.slice(9, -3);
+
+			return {
+				meta: metadata,
+				path: postPath
+			};
+		})
+	);
+
+	return allPosts;
+};
 
 export const GET = async () => {
-  const allPosts = await fetchMarkdownPosts()
+	const allPosts = await fetchMarkdownPosts();
 
-  const sortedPosts = allPosts.sort((a, b) => {
-    return new Date(b.meta.date) - new Date(a.meta.date)
-  })
+	const sortedPosts = allPosts.sort((a, b) => {
+		return new Date(b.meta.date) - new Date(a.meta.date);
+	});
 
-  return json(sortedPosts)
-}
+	return json(sortedPosts);
+};
