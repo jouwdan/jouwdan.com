f86e9c9be51869b8289dd4d7e00e9957d59a704b
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-04-10 20:19:04 +0100

    feat: tidy code

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,11 +1,10 @@
 import { json } from '@sveltejs/kit';
 
-const fetchMarkdownPosts = async () => {
+const fetchPosts = async () => {
 	const allPostFiles = import.meta.glob('$lib/blog/*.md');
-	const iterablePostFiles = Object.entries(allPostFiles);
 
 	const allPosts = await Promise.all(
-		iterablePostFiles.map(async ([path, resolver]) => {
+		Object.entries(allPostFiles).map(async ([path, resolver]) => {
 			const { metadata } = await resolver();
 			const postPath = path.slice(9, -3);
 
@@ -20,7 +19,7 @@ const fetchMarkdownPosts = async () => {
 };
 
 export const GET = async () => {
-	const allPosts = await fetchMarkdownPosts();
+	const allPosts = await fetchPosts();
 
 	const sortedPosts = allPosts.sort((a, b) => {
 		return new Date(b.meta.date) - new Date(a.meta.date);
